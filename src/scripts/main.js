1b9e6ede971304045e39fb20e52ffc7ef4171b10
BadOfficer <tarasbondarenko2005@gmail.com>
2025-08-28 18:56:05 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,73 @@
 'use strict';
 
-// write code here
+const appendRowBtn = document.querySelector('.append-row');
+const removeRowBtn = document.querySelector('.remove-row');
+const appendColBtn = document.querySelector('.append-column');
+const removeColBtn = document.querySelector('.remove-column');
+const table = document.querySelector('.field');
+
+changeRowBtnsState();
+changeColBtnsState();
+
+appendRowBtn.addEventListener('click', () => {
+  if (table.rows.length >= 10) {
+    return;
+  }
+
+  const newRowCopy = table.rows[0].cloneNode(true);
+
+  table.tBodies[0].append(newRowCopy);
+
+  changeRowBtnsState();
+});
+
+appendColBtn.addEventListener('click', () => {
+  if (table.rows[0].cells.length >= 10) {
+    return;
+  }
+
+  const newCol = document.createElement('td');
+  const tableRows = table.rows;
+
+  [...tableRows].forEach((row) => {
+    const colCopy = newCol.cloneNode();
+
+    row.append(colCopy);
+  });
+
+  changeColBtnsState();
+});
+
+removeRowBtn.addEventListener('click', () => {
+  if (table.rows.length <= 2) {
+    return;
+  }
+
+  table.rows[table.rows.length - 1].remove();
+
+  changeRowBtnsState();
+});
+
+removeColBtn.addEventListener('click', () => {
+  if (table.rows[0].cells.length <= 2) {
+    return;
+  }
+
+  const tableRows = table.rows;
+
+  [...tableRows].forEach((row) => {
+    row.cells[row.cells.length - 1].remove();
+  });
+
+  changeColBtnsState();
+});
+
+function changeRowBtnsState() {
+  appendRowBtn.disabled = table.rows.length >= 10;
+  removeRowBtn.disabled = table.rows.length <= 2;
+}
+
+function changeColBtnsState() {
+  appendColBtn.disabled = table.rows[0].cells.length >= 10;
+  removeColBtn.disabled = table.rows[0].cells.length <= 2;
+}
